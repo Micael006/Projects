c2ba57a6c6314795308612fab00424d1ec4bc1fb
Micael006 <58003787+Micael006@users.noreply.github.com>
2019-12-27 10:59:42 +0300

    Add files via upload

---
+++ kp3/kp3_2.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<math.h>
+double m_eps()
+{
+	double e = 1.0;
+	while (1.0 + e / 2.0 > 1.0){
+		e /= 2.0;
+	}
+	return e;
+}
+/*double power(double x, long n){
+	double a=1;
+	while(n){
+		if(n%2){
+			a*=x;
+			n--;
+		}
+		else{
+			x*=x;
+			n/=2;
+		}
+	}
+	return a;
+}*/
+double row(float x){
+	double result = -0.2, copy;
+	for(int i = 2; i <= 100; i++){
+		if((pow(2,i-1)/pow(5,i)*pow(x,i-1)) > m_eps()){
+		result -= pow(2,i-1)/pow(5,i)*pow(x,i-1);
+		}
+		else{
+		return result;
+		}
+	}		
+}
+double system_answer(float x){
+	return 1/(2*x-5);
+}
+void print_n_times_border(int size) {
+	printf("%.*s\n", size, "------------------------------------------------------------------------------");
+}
+void print_v(double x, double row, double system_answer, double r) {
+	printf("| %6.3lf | %6.3lf | %6.3lf | %6.3lf |\n", x, row, system_answer, r);
+	print_n_times_border(8 * 4 + 5);
+}
+int main(){
+	float piece = 0.125;
+	float now;
+	print_n_times_border(37);
+	for (int i = 0; i <= (int)(2/piece) ; i++) {
+		now = piece * i;
+		print_v(now, system_answer(now), row(now), system_answer(now)-row(now));
+	}
+	printf("Machine epsilone = %.11e\n", m_eps());
+	return 0;
+}
